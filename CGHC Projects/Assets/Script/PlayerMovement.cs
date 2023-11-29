ba5ec82fdbc51b3f700844f65e7f01e1e1e64454
DanyYap <124513296+DanyYap@users.noreply.github.com>
2023-11-29 10:19:11 +0800

    SetMousePointerScript

--- CGHC Projects/Assets/Script/PlayerMovement.cs
+++ CGHC Projects/Assets/Script/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
 
     public Vector2 movement;
+    public Vector2 mousePos;    
+
+    public Camera cam;
 
   
     // Update is called once per frame
@@ -16,10 +19,15 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement*moveSpeed *  Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
